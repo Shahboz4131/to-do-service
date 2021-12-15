dd3b9b9dea95d8520c076e5fdda02b951477abda
Shahboz4131 <shahbozkuchkorov4131@gmail.com>
2021-12-15 16:16:06 +0500

    additional

--- storage/postgres/task_test.go
+++ storage/postgres/task_test.go
@@ -7,47 +7,46 @@ import (
 	pb "github.com/Shahboz4131/to-do-service/genproto"
 )
 
-// func TestTaskRepo_Create(t *testing.T) {
-// 	tests := []struct {
-// 		name    string
-// 		input   pb.Task
-// 		want    pb.Task
-// 		wantErr bool
-// 	}{
-// 		{
-// 			name: "successful",
-// 			input: pb.Task{
-// 				Assignee: "Rustam",
-// 				Title:    "Turgunov",
-// 				Summary:  "summmary",
-// 				Deadline: "2020-10-10",
-// 				Status:   "active",
-// 			},
-// 			want: pb.Task{
-// 				Assignee: "Rustam",
-// 				Title:    "Turgunov",
-// 				Summary:  "summmary",
-// 				Deadline: "2020-10-10",
-// 				Status:   "active",
-// 			},
-// 			wantErr: false,
-// 		},
-// 	}
+func TestTaskRepo_Create(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   pb.Task
+		want    pb.Task
+		wantErr bool
+	}{
+		{
+			name: "successful",
+			input: pb.Task{
+				Assignee: "Rustam",
+				Title:    "Turgunov",
+				Summary:  "summmary",
+				Deadline: "2020-10-10",
+				Status:   "active",
+			},
+			want: pb.Task{
+				Assignee: "Rustam",
+				Title:    "Turgunov",
+				Summary:  "summmary",
+				Deadline: "2020-10-10T00:00:00Z",
+				Status:   "active",
+			},
+			wantErr: false,
+		},
+	}
 
-// 	for _, tc := range tests {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			got, err := pgRepo.Create(tc.input)
-// 			if err != nil {
-// 				t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.wantErr, err)
-// 			}
-// 			got.Id = 0
-// 			// got.Deadline = got.Deadline[:len(got.Deadline)-10]
-// 			if !reflect.DeepEqual(tc.want, got) {
-// 				t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.want, got)
-// 			}
-// 		})
-// 	}
-// }
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := pgRepo.Create(tc.input)
+			if err != nil {
+				t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.wantErr, err)
+			}
+			got.Id = 0
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.want, got)
+			}
+		})
+	}
+}
 
 func TestTaskRepo_Get(t *testing.T) {
 	tests := []struct {
