29eccbdcf0cc5fd833858e15b6fb1a231dc3529c
Shahboz4131 <shahbozkuchkorov4131@gmail.com>
2021-12-15 16:25:15 +0500

    additional

--- storage/postgres/task_test.go
+++ storage/postgres/task_test.go
@@ -32,6 +32,24 @@ func TestTaskRepo_Create(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name: "unsuccessful",
+			input: pb.Task{
+				Assignee: "Rustammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm",
+				Title:    "Turgunov",
+				Summary:  "summmary",
+				Deadline: "2020-10-10",
+				Status:   "active",
+			},
+			want: pb.Task{
+				Assignee: "Rustammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm",
+				Title:    "Turgunov",
+				Summary:  "summmary",
+				Deadline: "2020-10-10T00:00:00Z",
+				Status:   "active",
+			},
+			wantErr: true,
+		},
 	}
 
 	for _, tc := range tests {
@@ -83,49 +101,3 @@ func TestTaskRepo_Get(t *testing.T) {
 		})
 	}
 }
-
-// func TestTaskRepo_List(t *testing.T) {
-// 	tests := []struct {
-// 		name    string
-// 		inputPage   int64
-// 		inputLimit   int64
-// 		want1    []*pb.Task
-// 		want2	int64
-// 		wantErr bool
-// 	}{
-// 		{
-// 			name: "successful",
-// 			inputPage:  2,
-// 			inputLimit:  2,
-// 			want1: []pb.Task{
-// 				Assignee: "Rustam",
-// 				Title:    "Turgunov",
-// 				Summary:  "summmary",
-// 				Deadline: "2020-10-10",
-// 				Status:   "active",
-// 			},
-// 			wantErr: false,
-// 		},
-// 	}
-
-// 	for _, tc := range tests {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			got, err := pgRepo.Get(input)
-// 			if err != nil {
-// 				t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.wantErr, err)
-// 			}
-// 			// got.Id = 0
-// 			// got.Deadline = got.Deadline[:len(got.Deadline)-10]
-// 			if !reflect.DeepEqual(tc.want, got) {
-// 				t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.want, got)
-// 			}
-// 		})
-// 	}
-// }
-// func TestUserRepo_Get(t *testing.T) {
-
-// }
-
-// func TestUserRepo_List(t *testing.T) {
-
-// }
