b0e896960dff2daf053a994315e89aa787b2f30d
Shahboz4131 <shahbozkuchkorov4131@gmail.com>
2021-12-16 08:32:05 +0500

    added

--- storage/postgres/task_test.go
+++ storage/postgres/task_test.go
@@ -94,10 +94,80 @@ func TestTaskRepo_Get(t *testing.T) {
 			if err != nil {
 				t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.wantErr, err)
 			}
-			// tc.want.Id = got.Id
 			if !reflect.DeepEqual(tc.want, got) {
 				t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.want, got)
 			}
+		})
+	}
+}
+
+func TestTaskRepo_Update(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   pb.Task
+		want    pb.Task
+		wantErr bool
+	}{
+		{
+			name: "successful",
+			input: pb.Task{
+				Id:       4,
+				Assignee: "Rustam",
+				Title:    "Turgunov",
+				Summary:  "summmary",
+				Deadline: "2020-10-10",
+				Status:   "active",
+			},
+			want: pb.Task{
+				Id:       4,
+				Assignee: "Rustam",
+				Title:    "Turgunov",
+				Summary:  "summmary",
+				Deadline: "2020-10-10T00:00:00Z",
+				Status:   "active",
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := pgRepo.Update(tc.input)
+			if err != nil {
+				t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.wantErr, err)
+			}
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.want, got)
+			}
+		})
+	}
+}
+
+func TestTaskRepo_Delete(t *testing.T) {
+	tests := []struct {
+		name  string
+		input int64
+		// want    pb.EmptyRes
+		wantErr bool
+	}{
+		{
+			name:  "successful",
+			input: 1,
+			// want:    pb.EmptyRes{},
+			wantErr: false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			err := pgRepo.Delete(tc.input)
+			if err != nil {
+				t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.wantErr, err)
+			}
+
+			// if !reflect.DeepEqual(tc.want, got) {
+			// 	t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.want, got)
+			// }
 		})
 	}
 }
